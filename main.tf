fb7c0fbbaddad68c42df22b69be1512efb8aa550
TNgonephetsy-insight <116840552+TNgonephetsy-insight@users.noreply.github.com>
2022-11-16 15:05:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,27 @@
+terraform {
+  required_version = ">= 0.14.8"
+  required_providers {
+    azurerm = {
+      source  = "registry.terraform.io/hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
 
