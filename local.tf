3426f789fdc94a443e8ce1da4cdeea4b8b0f95f5
Tngonephetsy-insight <Thavone.Ngonephetsy@insight.com>
2022-11-17 14:45:56 -0500

    updating, yes

---
+++ local.tf
@@ -0,0 +1,5 @@
+local {
+    tags = {
+    environment = "staging"
+  }
+}
--- main.tf
+++ main.tf
@@ -6,6 +6,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+  cloud {
+    organization = "insight"
+
+    workspaces {
+      name = "tngonephetsy-terraform-training"
+    }
+  }
 }
 
 provider "azurerm" {
@@ -25,3 +32,12 @@ resource "azurerm_resource_group" "main" {
   location = var.location
 }
 
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = local.tags
+}
